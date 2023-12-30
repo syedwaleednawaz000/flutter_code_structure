e1179832e6f3e8aa1d643fececad4005c6d14279
syedwaleednawaz000 <syedwaleednawaz333@gmail.com>
2023-12-30 12:07:42 +0500

    by waleed fix theme , localization , go Rout

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:get/get.dart';
 import 'package:provider/provider.dart';
 import 'package:simple_flutter_project/Domain/counterprovider.dart';
 import 'package:simple_flutter_project/Domain/localization_provider.dart';
@@ -10,9 +10,7 @@ import 'package:simple_flutter_project/Domain/theme_provider.dart';
 import 'package:simple_flutter_project/Domain/userprovider.dart';
 import 'package:simple_flutter_project/Presentation/routes/app_route_configs.dart';
 import 'package:simple_flutter_project/l10n/support_languages.dart';
-import 'Presentation/Screens/Main/selectview.dart';
 import 'Presentation/Theme/theme_data.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized(); // Ensure widgets are initialized
