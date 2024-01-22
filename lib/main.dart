170ed625fd52dda3e91224f3746bd39b0297e511
syedwaleednawaz000 <syedwaleednawaz333@gmail.com>
2024-01-22 10:25:52 +0500

    by waleed solved go route issue

--- lib/main.dart
+++ lib/main.dart
@@ -4,20 +4,23 @@ import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
+import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'package:simple_flutter_project/Presentation/Screens/Home/Provider/counter_provider.dart';
+import 'package:simple_flutter_project/Presentation/Screens/Home/View/homeview.dart';
 import 'package:simple_flutter_project/Presentation/Screens/HomeNew/Provider/user_provider.dart';
 import 'package:simple_flutter_project/Presentation/Screens/SplashScreen/Provider/splash_provider.dart';
 import 'package:simple_flutter_project/Presentation/Screens/api/firebase_api.dart';
 import 'package:simple_flutter_project/Presentation/Theme/Provider/theme_provider.dart';
 import 'package:simple_flutter_project/config/app_constant.dart';
+import 'package:simple_flutter_project/config/app_router_constants.dart';
 import 'package:simple_flutter_project/l10n/Provider/localization_provider.dart';
+import 'Presentation/Screens/HomeNew/View/home_data_get_from_api_consumer.dart';
+import 'Presentation/Screens/HomeNew/View/home_data_get_from_api_selector.dart';
 import 'Presentation/Screens/Nav_bar_drawer/View/navigation_drawer_main_screen.dart';
+
 import 'Presentation/routes/app_route_configs.dart';
 import 'firebase_options.dart';
-//Todo by waleed
-// final navigatorKey = GlobalKey<NavigatorState>();
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -29,12 +32,12 @@ void main() async {
 }
 
 class MyApp extends StatelessWidget {
-  // final String? locale;
-
-  const MyApp({super.key,});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final navigatorKey = GlobalKey<NavigatorState>();
+
     return MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (_) => CounterProvider()),
@@ -43,45 +46,44 @@ class MyApp extends StatelessWidget {
         ChangeNotifierProvider(create: (_) => LanguageChangeProvider()),
         ChangeNotifierProvider(create: (_) => SplashProvider()),
       ],
-      child: Builder(builder: (BuildContext context) {
-        final languageProvider = Provider.of<LanguageChangeProvider>(context);
-        return Consumer<ThemeProvider>(
-          builder: (context, value, child) {
-            languageProvider.loadLocalLanguageCode();
-            return ScreenUtilInit(
-              designSize: const Size(393, 852),
-              minTextAdapt: true,
-              splitScreenMode: true,
-              builder: (context, child) {
-                final themeProvider = Provider.of<ThemeProvider>(context);
-                return MaterialApp.router(
-                  routerDelegate: MyAppRouter().router.routerDelegate,
-                  routeInformationParser: MyAppRouter().router.routeInformationParser,
-                  //Todo by waleed
-                  // navigatorKey: navigatorKey,
-                  debugShowCheckedModeBanner: false,
-                  title: 'Flutter code structure',
-                  theme: themeProvider.getTheme(),
-                  locale: languageProvider.appLocale,
-                  localizationsDelegates: const [
-                    AppLocalizations.delegate,
-                    GlobalMaterialLocalizations.delegate,
-                    GlobalWidgetsLocalizations.delegate,
-                    GlobalCupertinoLocalizations.delegate,
-                  ],
-                  supportedLocales: const [
-                    Locale('en'), // English
-                    Locale('ur'),
-                  ],
-                  // home: const NavigationDrawerMainScreen(),
-                );
-              },
-            );
-          },
-        );
-      }),
+      child: Builder(
+        builder: (BuildContext context) {
+          final languageProvider = Provider.of<LanguageChangeProvider>(context);
+
+          return Consumer<ThemeProvider>(
+            builder: (context, value, child) {
+              languageProvider.loadLocalLanguageCode();
+              return ScreenUtilInit(
+                designSize: const Size(393, 852),
+                minTextAdapt: true,
+                splitScreenMode: true,
+                builder: (context, child) {
+                  final themeProvider = Provider.of<ThemeProvider>(context);
+                  final router = MyAppRouter().router;
+                  return MaterialApp.router(
+                    // navigatorKey: navigatorKey,
+                    routerConfig: router,
+                    debugShowCheckedModeBanner: false,
+                    title: 'Flutter code structure',
+                    theme: themeProvider.getTheme(),
+                    locale: languageProvider.appLocale,
+                    localizationsDelegates: const [
+                      AppLocalizations.delegate,
+                      GlobalMaterialLocalizations.delegate,
+                      GlobalWidgetsLocalizations.delegate,
+                      GlobalCupertinoLocalizations.delegate,
+                    ],
+                    supportedLocales: const [
+                      Locale('en'), // English
+                      Locale('ur'),
+                    ],
+                  );
+                },
+              );
+            },
+          );
+        },
+      ),
     );
   }
 }
-
-
